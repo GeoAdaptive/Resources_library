c9ca1ae34b5f67ee5af37da45d9a15c19f42a80d
wenhaowuuu <wenhaow@upenn.edu>
2017-11-30 10:14:17 -0500

    1130_1

--- Example_Paraguay/js/js.js
+++ Example_Paraguay/js/js.js
@@ -1,12 +1,14 @@
-///1. SETUP AND BASEMAP
+///1. Setting up the Basemap
 
-//1.1 SETUP BASEMAP
 //SOME NOTES HERE
 var map = L.map('map', {
-  center: [-57.731, -24.317],
-  zoom: 13.5
+  center: [-23.817, -55.731],
+  zoom: 7
 });
 
+// other styles are also available to choose from
+// see here:
+// also here maybe: http://leaflet-extras.github.io/leaflet-providers/preview/
 // var Style = 'dark';
 var Style = 'light';
 
@@ -15,3 +17,11 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/'+ Style + '_all/{z}/{x}/{y}@2x.pn
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
   subdomains: 'abcd'
 }).addTo(map);
+
+///2. Adding the markers
+L.marker([-25.262, -57.581]).addTo(map)
+    .bindPopup('Asuncion, Paraguay')
+    .addTo(map);
+
+///3. Adding the layer data to be mapped
+var Departments = "https://raw.githubusercontent.com/wenhaowuuu/ProjectDataX/master/data/nt_muni_joined_clean.geojson";
--- README.md
+++ README.md
@@ -2,16 +2,27 @@
 
 Welcome to the Resources Library of GeoAdaptive!
 
-<strong>Learning Objectives</strong>
+<html>
+  <body>
+    <head>
+    </head>
 
-<ul>
-  <li> Moving on web </li>
-  <li> javascript </li>
-  <li> HTML </li>
-  <li> CSS </li>
-</ul>
-This is the first tutorial for GA learning
+    <strong>Learning Objectives</strong>
+      To learn how to use Github, the open collaboration platform for application development;
+      To learn the basics of javascript, HTML, and CSS;
+      To learn how to set up a simple web-mapping application;
 
-# putting up a web-map
+    <ul>
+      <li> Moving on web </li>
+      <li> javascript </li>
+      <li> HTML </li>
+      <li> CSS </li>
+    </ul>
+    This is the first tutorial for GA learning
 
-<h2>Socio-economic Conditions in Pararguay </h2>
+    # putting up a web-map
+
+    <h2>Socio-economic Conditions in Pararguay </h2>
+
+    </body>
+</html>
